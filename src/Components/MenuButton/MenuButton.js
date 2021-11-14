5628e9b7ffb42c0d685a383e8de2937e2a85cd41
James <ball.j.work@gmail.com>
2021-11-14 18:57:32 +0000

    options menu transition added

--- src/Components/MenuButton/MenuButton.js
+++ src/Components/MenuButton/MenuButton.js
@@ -1,20 +1,31 @@
 import React, {  useState } from "react";
 import OptionsMenu from "../Options-Menu/OptionsMenu";
 import { ReactComponent as MenuIcon } from './Menu-Icons/three-dots-vertical.svg'
+import { CSSTransition } from "react-transition-group";
 
 
 const MenuButton = () => {
     
     const [ display, setDisplay ] = useState(false);
-
+    
+    
     return (
         <div>
             <button aria-label='menu button' className='menu-btn' onClick={() => setDisplay(!display)}>
                 <MenuIcon aria-lable='menu button icon' className='menu' />
             </button>
-            {display && <OptionsMenu id='options-menu' />}
+            <CSSTransition 
+                            in={display} 
+                            timeout={400} 
+                            classNames={'options-menu'} 
+                            mountOnEnter={false}
+                            unmountOnExit={true}
+                            >
+                <OptionsMenu />
+            </CSSTransition>    
         </div>
     )
-}
+}    
+
 
 export default MenuButton;
--- src/Components/Options-Menu/OptionsMenu.js
+++ src/Components/Options-Menu/OptionsMenu.js
@@ -4,23 +4,24 @@ import { ReactComponent as NightMode } from './Icons/NightMode.svg';
 import { ReactComponent as Github } from './Icons/Github.svg';
 import { ReactComponent as RedditIcon } from './Icons/RedditIcon.svg';
 
-const OptionsMenu = ({ style }) => {
+const OptionsMenu = () => {
     return (
-        <div aria-label='site options menu' className='options-menu' style={style}>
-            <span className='options-text light-grey veiw-options'> View options </span>
-            <NightMode className='icon nightmode-icon' />
-            <span className='options-text nightmode-text'>Night Mode</span>
-            <label id='nightmode-slider' className="nightmode-slider" htmlFor="checkbox">
-                <input aria-labelledby='nightmode-slider' type="checkbox" id="checkbox" />
-                <div aria-labelledby='nightmode-slider' className="round slider"></div>
-            </label>
-            <span className='options-text light-grey more-stuff'> More Stuff </span> 
-            <Github alt='github icon' className='icon github-icon' />
-            <span className='options-text github-text'>Github</span>
-            <RedditIcon alt='github icon' className='icon reddit-icon' />
-            <span className='options-text reddit-text'>Visit Reddit</span> 
-        </div>
-    )
+            <div aria-label='site options menu' className='options-menu'>
+                <span className='options-text light-grey veiw-options'> View options </span>
+                <NightMode className='icon nightmode-icon' />
+                <span className='options-text nightmode-text'>Night Mode</span>
+                <label id='nightmode-slider' className="nightmode-slider" htmlFor="checkbox">
+                    <input aria-labelledby='nightmode-slider' type="checkbox" id="checkbox" />
+                    <div aria-labelledby='nightmode-slider' className="round slider"></div>
+                </label>
+                <span className='options-text light-grey more-stuff'> More Stuff </span> 
+                <Github alt='github icon' className='icon github-icon' />
+                <span className='options-text github-text'>Github</span>
+                <RedditIcon alt='github icon' className='icon reddit-icon' />
+                <span className='options-text reddit-text'>Visit Reddit</span> 
+            </div>
+        )
+      
 }
 
 export default OptionsMenu;
