dd83a1ce49488f7ca059877dbbe6f449ebfd03a8
James <ball.j.work@gmail.com>
2021-11-17 21:59:35 +0000

    post card styling wip

--- src/Components/Posts/PostCard.js
+++ src/Components/Posts/PostCard.js
@@ -8,20 +8,23 @@ const PostCard = () => {
 
     return (
         <div data-testid='post-card' className='post-card'>
-            <div className='post-card-title-container'>
-                <div className='post-card-img-container'><img className='post-card-img' /></div>
-                <span className='post-card-link'>r/AskReddit</span>
-                <span className='post-card-posted-by'>Posted by u/React-Tesing-Libary</span>
+            <div className='post-card-container'>
+            <header className='post-card-title'>
+                    <div className='post-card-img-container'><img className='post-card-img' /></div>
+                    <span className='post-card-link'>r/AskReddit</span>
+                    <span className='post-card-posted-by'>Posted by u/React-Tesing-Libary</span>
+                </header>
+                <h2 className='post-card-preview'>What’s the best testing libary for a React / Redux app?</h2>
+                <footer className='post-card-footer'>
+                    <div className='post-card-comments'><MdComment /><span className='post-card-text'>Comments</span></div>
+                    <div className='post-card-details'><BiLinkExternal /><span className='post-card-text'> Details </span></div>
+                    <div className='post-card-share'><BsShareFill /><span className='post-card-text'> Share </span></div>
+                </footer>
             </div>
-            <span className='post-card-content-preview'>What’s the best testing libary for a React / Redux app?</span>
-            <div className='comment-details-share container'>
-                <div className='post-card-comments'><MdComment /><span className='post-card-text'>Comments</span></div>
-                <div className='post-card-details'><BiLinkExternal /><span className='post-card-text'> Details </span></div>
-                <div className='post-card-share'><BsShareFill /><span className='post-card-text'> Share </span></div>
-            </div>
-
             <div className='post-card-media-preview'> Media</div>
-            <div className='post-card-votes'>27k</div>
+            <aside className='post-card-votes'>
+                <span>27k</span>
+            </aside>
         </div>
     );
 };
