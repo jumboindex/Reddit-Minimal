0e9956a0c8aefe272acb9f6f1b679f0f52052cef
James <ball.j.work@gmail.com>
2021-11-18 12:25:12 +0000

    post card styling completed

--- src/Components/Posts/PostCard.js
+++ src/Components/Posts/PostCard.js
@@ -9,21 +9,24 @@ const PostCard = () => {
     return (
         <div data-testid='post-card' className='post-card'>
             <div className='post-card-container'>
-            <header className='post-card-title'>
+                <header className='post-card-title'>
                     <div className='post-card-img-container'><img className='post-card-img' /></div>
                     <span className='post-card-link'>r/AskReddit</span>
                     <span className='post-card-posted-by'>Posted by u/React-Tesing-Libary</span>
                 </header>
-                <h2 className='post-card-preview'>What’s the best testing libary for a React / Redux app?</h2>
-                <footer className='post-card-footer'>
-                    <div className='post-card-comments'><MdComment /><span className='post-card-text'>Comments</span></div>
-                    <div className='post-card-details'><BiLinkExternal /><span className='post-card-text'> Details </span></div>
-                    <div className='post-card-share'><BsShareFill /><span className='post-card-text'> Share </span></div>
-                </footer>
+
+                <div className='post-card-content'>
+                    <h2 className='post-card-preview'>What’s the best testing libary for a React / Redux app?</h2>
+                    <footer className='post-card-footer'>
+                        <div className='post-card-actions'><MdComment className='post-card-icon' /><span className='post-card-text'>Comments</span></div>
+                        <div className='post-card-actions'><BiLinkExternal className='post-card-icon' /><span className='post-card-text'> Details </span></div>
+                        <div className='post-card-actions'><BsShareFill className='post-card-icon' /><span className='post-card-text'> Share </span></div>
+                    </footer>
+                </div>
             </div>
             <div className='post-card-media-preview'> Media</div>
             <aside className='post-card-votes'>
-                <span>27k</span>
+                <h2>27k</h2>
             </aside>
         </div>
     );
