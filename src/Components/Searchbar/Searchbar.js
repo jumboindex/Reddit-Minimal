0cebb4d534c5d6e40d286eef9b28ac083d300b9d
James <ball.j.work@gmail.com>
2021-12-02 17:33:07 +0000

    adding spinner to search bar wip

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import './Searchbar.css';
+import Spinner from 'react-bootstrap/Spinner'
+import { FcSearch } from 'react-icons/fc'
 
 
 const Searchbar = () => {
@@ -20,12 +22,16 @@ const Searchbar = () => {
     }, [searchText]);
 
     return ( 
-        <div data-testid='searchbar' className='search-container'>
+        <div data-testid='searchbar' className='search-container' tabIndex='1'>
             <input className='searchbar' 
                     type='text' 
                     placeholder={searchText} 
                     onKeyPress={(e) => setSearchText(e.target.value)}
                     />
+            <Spinner animation="border" role="status">
+                {/* <span className="visually-hidden">Loading...</span> */}
+            </Spinner>     
+            <FcSearch className='search-icon' />
         </div>
         
     )
