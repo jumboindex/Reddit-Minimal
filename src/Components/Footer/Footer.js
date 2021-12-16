8b4c121a3c9f5dee939d2bb1bffce2be95de9d1c
James <ball.j.work@gmail.com>
2021-12-16 13:53:26 +0000

    hyperlinks updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
        
             <footer className='app-footer'>
                <a href='https://github.com/jumboindex/Reddit-Minimal' target='_blank' rel="noreferrer"> About </a>
-               |<a href='' target='_blank' rel="noreferrer"> License </a>
+               |<a href='https://github.com/jumboindex/Reddit-Minimal/blob/main/LICENSE.md' target='_blank' rel="noreferrer"> License </a>
                | <a href='https://github.com/jumboindex/Reddit-Minimal/issues' target='_blank' rel="noreferrer"> Issues </a>
                | <a href='https://github.com/jumboindex/Reddit-Minimal/labels/backlog' target='_blank' rel="noreferrer"> Backlog </a>
             </footer>
